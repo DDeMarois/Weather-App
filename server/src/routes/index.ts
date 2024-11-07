2fbc65de1c9c8fe16202f43941d3dbcb24f47939
DDeMarois <Drew.DeMarois@du.edu>
2024-11-06 23:24:10 -0700

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,9 +2,15 @@ import { Router } from 'express';
 const router = Router();
 
 import apiRoutes from './api/index.js';
+import path from 'path';
 import htmlRoutes from './htmlRoutes.js';
 
 router.use('/api', apiRoutes);
+
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/public/index.html'));
+});
+
 router.use('/', htmlRoutes);
 
 export default router;
