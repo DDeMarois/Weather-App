a8947162f767feca29db0569410f14d438e95942
DDeMarois <Drew.DeMarois@du.edu>
2024-11-06 22:49:21 -0700

    Update htmlRoutes.ts

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -5,11 +5,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-router.use(express.static(path.join(__dirname, 'public')));
+router.use(express.static(path.resolve(__dirname, '../../public')));
 import { Request, Response } from 'express';
 
 router.get('/', (_req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../public', 'index.html'));
 });
 
 export default router;
